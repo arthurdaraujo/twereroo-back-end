1cebe461790f1e0a4e4465061826bc31cf49fafd
Arthur Araujo <arthur.araujo.de@gmail.com>
2025-01-21 15:50:36 -0300

    post route complete

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 import express from "express";
 import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const users = []
+
+app.post("/sing-up", (req, res) => {
+    const  {username, avatar}  = req.body
+
+    if(users.find(user => user.username === username)) {
+        res.status(409).send("conflict")
+        return
+    }
+
+    users.push({
+        id: users.length +1,
+        username,
+        avatar
+    })
+
+    //console.log(users);
+    res.send(users);
+})
+
+
+app.listen(5000, () => console.log("server in port 5000"))
