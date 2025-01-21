15b79e26dd3f1f7edd7a2efc14617d8ea21d2c28
Arthur Araujo <arthur.araujo.de@gmail.com>
2025-01-21 16:43:49 -0300

    tweets post route complete

--- index.js
+++ index.js
@@ -6,8 +6,95 @@ app.use(cors());
 app.use(express.json());
 
 const users = []
+const tweets = [
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miau"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miau"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miau"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miau"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miau"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miau"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miau"
+    },
+    {	
+        username: "decimo",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miau"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miau"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miaujinha"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "piupiujinha"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "miaujinho lindo"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu como cocô"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "maconha é melhor do que cocaína"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "cocozao azedo com maiosese"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "bosta enlatada"
+    },
+    {	
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "maconha é melhor do que o crack"
+    }
+]
 
-app.post("/sing-up", (req, res) => {
+app.post("/sign-up", (req, res) => {
     const  {username, avatar}  = req.body
 
     if(users.find(user => user.username === username)) {
@@ -25,5 +112,20 @@ app.post("/sing-up", (req, res) => {
     res.send(users);
 })
 
+app.get("/tweets", (req, res) => {
+   
+    console.log(typeof tweets.length)
+
+    if(tweets.length > 10) {
+        const lastTweets = tweets.length - 10
+
+        const last10Tweets = tweets.filter((tweet, i) => i >= lastTweets )
+        console.log(last10Tweets)
+        res.send(last10Tweets)
+        return
+    }
+
+    res.send(tweets)
+})
 
 app.listen(5000, () => console.log("server in port 5000"))
